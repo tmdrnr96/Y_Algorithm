f934334aca0e4c389bdbd53a567d74ccab804b00
yooseungkook <tmdrnr96@naver.com>
2020-12-15 23:25:06 +0900

    BeakJoon

---
+++ No_4/src/no_4/No_10951_1.java
@@ -0,0 +1,29 @@
+package no_4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class No_10951_1 {
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		String str;
+		
+		while((str = br.readLine()) != null) {
+			
+			st = new StringTokenizer(str," ");
+			
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			sb.append(a+b).append("\n");
+
+		}
+		System.out.println(sb);
+	
+		br.close();
+	}//main
+}
